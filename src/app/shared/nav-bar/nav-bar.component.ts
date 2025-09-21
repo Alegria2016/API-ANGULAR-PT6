cc68561696bc8b2697e8d5453daf21faab02aeaa
felix alegria <felix.alegria@bjungle.net>
2025-09-20 21:29:20 -0500

    Documentación README.

--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+   
+  ],
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
